2829d993bee7e1a4c9899e13ef3b2899f089fcaf
Ibrahim Mohsen <ibrahim.mohsen.zaki@gmail.com>
2019-12-16 21:28:45 +0200

    init add of project

---
+++ src/main/java/com/springframework/joke/JokesAppApplication.java
@@ -0,0 +1,13 @@
+package com.springframework.joke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JokesAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JokesAppApplication.class, args);
+    }
+
+}
