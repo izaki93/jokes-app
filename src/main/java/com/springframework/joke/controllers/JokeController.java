707bcb28a089d2f19ff73c12af03e6cc964eda43
Ibrahim Mohsen <ibrahim.mohsen.zaki@gmail.com>
2019-12-16 22:12:04 +0200

    modify getJoke requst mapping

--- src/main/java/com/springframework/joke/controllers/JokeController.java
+++ src/main/java/com/springframework/joke/controllers/JokeController.java
@@ -16,7 +16,7 @@ public class JokeController {
         this.jokeService = jokeService;
     }
 
-    @RequestMapping("/")
+    @RequestMapping({"/",""})
     public String showJoke(Model model){
         model.addAttribute("joke",jokeService.getJoke());
 
