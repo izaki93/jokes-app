cfeae5bc4295030e92a9b14940609f6a7025c6b0
Ibrahim Mohsen <ibrahim.mohsen.zaki@gmail.com>
2019-12-16 22:47:35 +0200

    Add joke view

--- src/main/java/com/springframework/joke/controllers/JokeController.java
+++ src/main/java/com/springframework/joke/controllers/JokeController.java
@@ -16,8 +16,9 @@ public class JokeController {
         this.jokeService = jokeService;
     }
 
-    @RequestMapping("/")
+    @RequestMapping({"/",""})
     public String showJoke(Model model){
+
         model.addAttribute("joke",jokeService.getJoke());
 
         return "chucknorris";
