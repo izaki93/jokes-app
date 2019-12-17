48c51b8276596877bd619be61ac1e3279760b7bc
izaki93 <ibrahim.mohsen.zaki@gmail.com>
2019-12-17 15:36:02 +0200

    adding spring xml based configuration.

--- src/main/java/com/springframework/joke/JokesAppApplication.java
+++ src/main/java/com/springframework/joke/JokesAppApplication.java
@@ -2,8 +2,10 @@ package com.springframework.joke;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:chuck-config.xml")
 public class JokesAppApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/springframework/joke/config/ChuckConfiguration.java
+++ src/main/java/com/springframework/joke/config/ChuckConfiguration.java
@@ -4,10 +4,10 @@ import guru.springframework.norris.chuck.ChuckNorrisQuotes;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+//@Configuration
 public class ChuckConfiguration {
 
-    @Bean
+    //@Bean
     public ChuckNorrisQuotes chuckNorrisQuotes(){
         return new ChuckNorrisQuotes();
     }
